7e0d77bd714050b6f1678a8b67e5192a589c391f
Thrasher <ajtroag@gmail.com>
2022-01-25 04:42:08 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from "@/lib/prisma";
+
 
 export default NextAuth({
     providers: [
@@ -10,5 +10,5 @@ export default NextAuth({
             clientSecret: process.env.DISCORD_CLIENT_SECRET
         })
     ],
-    adapter: PrismaAdapter(prisma)
+
 })
